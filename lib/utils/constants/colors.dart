c037bd508b370c31c67a41c720776f82a84953a0
Bodan07 <najmi.kamil@gmail.com>
2024-03-26 11:58:16 +0700

    assets

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GColors {
+  GColors._();
+
+  // Background colors
+  static const Color primaryBackground = Color(0x3A4D39);
+  static const Color secondaryBackground = Color(0x4F6F52);
+
+  // Text Colors
+  static const Color textTitle = Color(0xFFECE3CE);
+  static const Color textPrimary = Color(0xFF3A4D39);
+  static const Color textSecondary = Color(0x3A4D39);
+  static const Color textTeritary = Color(0x333333);
+  static const Color textWhite = Colors.white;
+}

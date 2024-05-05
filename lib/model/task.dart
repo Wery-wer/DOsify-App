6cb6a15536c03dc3a711d0f4459366a7daff4156
Bodan07 <najmi.kamil@gmail.com>
2024-05-05 19:55:34 +0700

    update

---
+++ lib/model/task.dart
@@ -0,0 +1,8 @@
+import 'dart:ffi';
+
+class Task {
+  String name;
+  bool status;
+
+  Task({this.name = "", this.status = false});
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class User {
+  String username;
+  String email;
+  String password;
+
+  User({this.username = "", this.email = "", this.password = ""});
+}

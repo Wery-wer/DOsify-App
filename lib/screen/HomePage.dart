b92e8c5f0adb0fd3d8aeabbac49b64c37ba12531
Wery <44234831+Wery-wer@users.noreply.github.com>
2024-05-24 10:41:46 +0700

    refresh

--- lib/screen/HomePage.dart
+++ lib/screen/HomePage.dart
@@ -2,7 +2,7 @@ import 'package:dosify_app/utils/constants/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:intl/intl.dart'; 
+import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:dosify_app/utils/constants/sizes.dart' as sizes;
 import 'package:dosify_app/widget/FragmentAddList.dart';
@@ -21,21 +21,15 @@ class _HomePageState extends State<HomePage> {
   bool? isChecked = false;
 
   final List<Map<String, dynamic>> gridMap = [
-    {
-      "title": "Travel"
-    },
-    {
-      "title": "Music"
-    },
-    {
-      "title": "Task"
-    },
+    {"title": "Travel"},
+    {"title": "Music"},
+    {"title": "Task"},
   ];
 
   List<String> logoFileNames = [
-  'logo1.png',
-  'logo2.png',
-  'logo3.png',
+    'logo1.png',
+    'logo2.png',
+    'logo3.png',
   ];
 
   String formattedDate = '';
@@ -46,16 +40,19 @@ class _HomePageState extends State<HomePage> {
     initializeDateFormatting();
     fetchDateAndTime();
   }
-  
+
   void addNewList(String newListName) {
     setState(() {
       data.add(newListName);
     });
   }
 
-  List<String> data = ["Travel", "Music", "Task"]; // template grid view builder di awal
+  List<String> data = [
+    "Travel",
+    "Music",
+    "Task"
+  ]; // template grid view builder di awal
   List<String> _tasks = [];
-
 
   void fetchDateAndTime() {
     var now = DateTime.now();
@@ -108,7 +105,7 @@ class _HomePageState extends State<HomePage> {
                       children: [
                         const Spacer(),
                         Text(
-                          'Add Task', 
+                          'Add Task',
                           style: const TextStyle(
                             fontFamily: 'Poppins',
                             color: Colors.white,
@@ -150,25 +147,28 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                ..._tasks.map((task) => Row(
-                      children: [
-                        Checkbox(
-                          value: true, // Atur nilai checkbox sesuai dengan keadaan tugas
-                          onChanged: (newValue) {
-                            // Perbarui keadaan tugas
-                          },
-                        ),
-                        Expanded(
-                          child: Text(
-                            task,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: sizes.GSizes.fontSizeMd,
+                ..._tasks
+                    .map((task) => Row(
+                          children: [
+                            Checkbox(
+                              value:
+                                  true, // Atur nilai checkbox sesuai dengan keadaan tugas
+                              onChanged: (newValue) {
+                                // Perbarui keadaan tugas
+                              },
                             ),
-                          ),
-                        ),
-                      ],
-                    )).toList(),
+                            Expanded(
+                              child: Text(
+                                task,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: sizes.GSizes.fontSizeMd,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ))
+                    .toList(),
                 SizedBox(height: 520),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -176,11 +176,10 @@ class _HomePageState extends State<HomePage> {
                     TextButton(
                       onPressed: () {
                         showDialog(
-                          context: context,
-                          builder: (context) {
-                            return const alertLogout();
-                          }
-                        );
+                            context: context,
+                            builder: (context) {
+                              return const alertLogout();
+                            });
                       },
                       child: Text(
                         'Delete List',
@@ -191,14 +190,14 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     ),
-                    FloatingActionButton( 
+                    FloatingActionButton(
                       onPressed: () {
                         setState(() {
-                              // Simpan task yang ditulis di dalam TextField ke dalam daftar
-                              _tasks.add(controller.text);
-                              // Kosongkan TextField setelah menyimpan tugas
-                              controller.clear();
-                            });
+                          // Simpan task yang ditulis di dalam TextField ke dalam daftar
+                          _tasks.add(controller.text);
+                          // Kosongkan TextField setelah menyimpan tugas
+                          controller.clear();
+                        });
                       },
                       child: Image.asset(
                         'assets/images/plus.png',
@@ -207,7 +206,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                       backgroundColor: GColors.fourht,
                     ),
-                ],
+                  ],
                 ),
               ],
             ),
@@ -216,7 +215,6 @@ class _HomePageState extends State<HomePage> {
       },
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -228,12 +226,11 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-              '$formattedDate',
-              style: const TextStyle(
-                fontFamily: 'Poppins', 
-                fontSize: sizes.GSizes.fontSizeLg,
-                color: Colors.white
-              ),
+                '$formattedDate',
+                style: const TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: sizes.GSizes.fontSizeLg,
+                    color: Colors.white),
               ),
               const Spacer(),
               IconButton(
@@ -254,18 +251,21 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                         child: Padding(
-                          padding: const EdgeInsets.all(18.0), 
+                          padding: const EdgeInsets.all(18.0),
                           child: Theme(
                             data: ThemeData(
                               primarySwatch: Colors.deepOrange,
-                              colorScheme: ColorScheme.light(primary: Color.fromARGB(255, 252, 71, 0)),
-                              buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
+                              colorScheme: ColorScheme.light(
+                                  primary: Color.fromARGB(255, 252, 71, 0)),
+                              buttonTheme: ButtonThemeData(
+                                  textTheme: ButtonTextTheme.primary),
                             ),
                             child: Container(
-                              height: 600, 
+                              height: 600,
                               decoration: const BoxDecoration(
                                 color: Color(0xFFFFF2D8),
-                                borderRadius: BorderRadius.all(Radius.circular(13)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(13)),
                               ),
                               child: CalendarDatePicker(
                                 initialDate: DateTime.now(),
@@ -288,96 +288,107 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: GColors.textPrimary,
       ),
-      body: Column(    
-        children: [ 
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(left: 14,right: 14),
-            decoration: const BoxDecoration(
-              color: GColors.primaryBackground,
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: GColors.textTitle,
-                contentPadding: const EdgeInsets.symmetric(vertical: 1, horizontal: 1),
-                prefixIcon: Image.asset(
-                  'assets/images/search.png',
-                  width: 20,
-                  height: 20,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: const BorderSide(
-                    color: Color(0xFF3A4D39),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          Navigator.pop(context);
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const HomePage()));
+        },
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(left: 14, right: 14),
+              decoration: const BoxDecoration(
+                color: GColors.primaryBackground,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: GColors.textTitle,
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 1, horizontal: 1),
+                  prefixIcon: Image.asset(
+                    'assets/images/search.png',
+                    width: 20,
+                    height: 20,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF3A4D39),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(11),
-              decoration: const BoxDecoration(
-                color: Color(0xFF3A4D39), 
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 12.0,
-                    mainAxisSpacing: 12.0,
-                  ), 
-                  itemCount: data.length,
-                  itemBuilder: (_, index){
-                    Random random = Random();
-                    String randomLogoFileName = logoFileNames[random.nextInt(logoFileNames.length)];
-                    String logoAssetPath = 'assets/logo/$randomLogoFileName';
-                    return GestureDetector(
-                      onTap: () {
-                        _showAddTaskModal(context);
-                    },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: const Color(0xFFFFF2D8),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              top: 8.0,
-                              left: 8.0,
-                              child: Image.asset(
-                                logoAssetPath,
-                                width: 30,
-                                height: 30,
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 8.0,
-                              left: 8.0,
-                              child: Text(
-                                data[index],
-                                style: const TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 17,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(11),
+                decoration: const BoxDecoration(
+                  color: Color(0xFF3A4D39),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 12.0,
+                        mainAxisSpacing: 12.0,
                       ),
-                    );
-                  }
+                      itemCount: data.length,
+                      itemBuilder: (_, index) {
+                        Random random = Random();
+                        String randomLogoFileName =
+                            logoFileNames[random.nextInt(logoFileNames.length)];
+                        String logoAssetPath =
+                            'assets/logo/$randomLogoFileName';
+                        return GestureDetector(
+                          onTap: () {
+                            _showAddTaskModal(context);
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFFFF2D8),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  top: 8.0,
+                                  left: 8.0,
+                                  child: Image.asset(
+                                    logoAssetPath,
+                                    width: 30,
+                                    height: 30,
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 8.0,
+                                  left: 8.0,
+                                  child: Text(
+                                    data[index],
+                                    style: const TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton( // Tombol Floating
+      floatingActionButton: FloatingActionButton(
+        // Tombol Floating
         onPressed: () {
           _showAddListModal(context);
         },
@@ -387,16 +398,17 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Color(0xFF3A4D39),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat, // Letakkan tombol di bawah kiri
+      floatingActionButtonLocation: FloatingActionButtonLocation
+          .endFloat, // Letakkan tombol di bawah kiri
     );
   }
 }
 
 //ini menampilkan nama yang sesuai dengan nama list dengan nama appbar nya tapi kayanya ga kepake
-class ItemContentWidget extends StatelessWidget{
+class ItemContentWidget extends StatelessWidget {
   final String data;
   ItemContentWidget({required this.data});
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
